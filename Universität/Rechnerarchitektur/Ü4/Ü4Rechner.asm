aba5adc03048ca6d24786454e9602dd57659c896
William Djalal <william.dj@outlook.de>
2017-11-23 18:52:10 +0100

    NEW

--- Universität/Rechnerarchitektur/Ü4/Ü4Rechner.asm
+++ Universität/Rechnerarchitektur/Ü4/Ü4Rechner.asm
@@ -2,63 +2,69 @@ section .text
 
 global formula
 
-formula:
-	ADD eax, ebx ;(a+b)
-	MOV ebx, eax
-	SUB ecx, edx ;(c-d)
-	MOV eax, 0
-	MOV eax, ecx
-	MOV edx, 0
-    IMUL ebx ;(a+b)*(c-d) = t1
-	MOV ebx, eax
-	MOV eax, 0
+;Bearbeitet von: Nils Kerk, William Djalal
 
-	MOV eax, esi
-	MOV esi, 0
-	MOV esi, 8
-	MOV edx, 0
-	IMUL esi ;e*8
-	MOV esi, eax
+formula:
+	ADD edi, esi ;(a+b)
+	SUB edx, ecx ;(c-d)
 	MOV eax, 0
-	
-	MOV eax, edi
-	MOV edi, 0
-	MOV edi, 4
+	MOV eax, edx
 	MOV edx, 0
-	IMUL edi ;f*4
+    IMUL edi ;(a+b)*(c-d) = t1
 	MOV edi, eax
 	MOV eax, 0
-	
-	MOV r8d, [rsp+8] ;Stack g
-	MOV r9d, [rsp+16] ;Stack h
-	
-	MOV eax, 2
+
+	MOV eax, r8d
+	MOV r8d, 0
+	MOV r8d, 8
 	MOV edx, 0
-	IDIV r8d ;g/2
+	IMUL r8d ;e*8
 	MOV r8d, eax
 	MOV eax, 0
 	
-	MOV eax, 4
+	MOV eax, r9d
+	MOV r9d, 0
+	MOV r9d, 4
 	MOV edx, 0
-	IDIV r9d ;h/4
+	IMUL r9d ;f*4
 	MOV r9d, eax
 	MOV eax, 0
 	
-	ADD esi, edi ;e+f
+	MOV esi, [rsp+8] ;Stack g
+	MOV ecx, [rsp+16] ;Stack h
 	
-	SUB esi, r8d ;(e+f)-g
-	
-	ADD esi, r9d ;((e+f)-g)+h
-
 	MOV eax, esi
 	MOV esi, 0
-	MOV esi, ebx
+	MOV esi, 2
 	MOV edx, 0
-	IMUL ebx ;t1*t2 = t3
+	CDQ
+	IDIV esi ;g/2
+	MOV esi, eax
+	MOV eax, 0
+	
+	MOV eax, ecx
+	MOV ecx, 0
+	MOV ecx, 4
+	MOV edx, 0
+	CDQ
+	IDIV ecx ;h/4
+	MOV ecx, eax
+	MOV eax, 0
+	
+	ADD r8d, r9d ;e+f
+	
+	SUB r8d, esi ;(e+f)-g
+	
+	ADD r8d, ecx ;((e+f)-g)+h
+
+	MOV eax, edi
+	MOV edx, 0
+	IMUL r8d ;t1*t2 = t3
 	
 	MOV edx, 0
 	MOV esi, 0
 	MOV esi, 3
+	CDQ
 	IDIV esi ;t/3
 	ret
 	
