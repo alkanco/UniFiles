899601d1415389f2f58a46fdc7dfc209f5cc2131
William Djalal <william.dj@outlook.de>
2017-11-27 18:36:44 +0100

    Monday (Laptop)

--- Universität/Funktionale Programmierung/Ü5/Ü5.hs
+++ Universität/Funktionale Programmierung/Ü5/Ü5.hs
@@ -1,13 +1,15 @@
+--Funktionale Programmierung-Übung5
 import Data.List (sort,nub)
+
 --1. Aufgabe
 toDecFrom :: Int -> [Int] -> Int
 toDecFrom b xs = foldl multBase 0 xs
-	where
-		multBase x y = (x*b)+y
---Eingabewert: n=Länge der Liste xs
---multBase ist konstant, kann daher als solche behandelt werden.
+  where
+    multBase x y = (x*b)+ y
+--Eingabewert: n = Länge der Liste xs
+--"multBase" ist konstant, kann daher als solche behandelt werden.
 --foldl ist von der Komplexität her linear.
---O(n)
+--Es gilt: O(n)
 
 --2. Aufgabe
 --flatten_r im Gegensatz zu flatten_l ist viel komplizierter, da von rechts nach links
@@ -17,15 +19,14 @@ flatten xss = [y | ys <- xss, y <- ys]
 
 --3. Aufgabe
 potenzSeq :: Int -> Int -> [Int]
-potenzSeq n m = sort nub [b^e|b<-[2,3..n],e<-[2,3..m]]
+potenzSeq n m = sort (nub [b^e | b <- [2,3..n], e <- [2,3..m]])
 --potenzSeq ist eine lineare Funktion
---O(n*m)
+--Es gilt: O(n*m)
 
 --4. Aufgabe
-{-
 minNatNotIn :: [Int] -> Int
-minNatNotIn xs = [y|y<-[0..5],y/=xs] -}
+minNatNotIn xs = minimum [y | z <- [last xs], y <- [0..z], notElem y xs]
 
 --5. Aufgabe
---Eingabewert: n=Länge der Liste xs
---O
+--Eingabewert: n = Länge der Liste xs
+--Es gilt: 
