c0d9d391cf82e2a1ee1656e203c58deab05163f9
William <william.dj@outlook.de>
2017-11-27 13:28:58 +0100

    Coole ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄnderung

--- Universität/Funktionale Programmierung/Ü5/Ü5.hs
+++ Universität/Funktionale Programmierung/Ü5/Ü5.hs
@@ -1,8 +1,14 @@
 import Data.List (sort)
+
+-- Aufgabe 1
+funktion b xs = foldl multBase 0 xs
+  where
+    multBase x y = x * b + y
+
 --3. Aufgabe
 potenzSeq :: Int -> Int -> [Int]
 potenzSeq n m = sort [b^e|b<-[2,3..n],e<-[2,3..m]]
 --potenzSeq ist eine quadratische Funktion
 
 --4. Aufgabe
-minNatNotIn [n] = [x|x<-[1..
+-- minNatNotIn [n] = [x|x<-[1..
