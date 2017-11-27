5784006f03b156328d47e8fe4614a6e10c327c8a
William <william.dj@outlook.de>
2017-11-27 15:54:21 +0100

    sollte alles clean sein?

--- Universität/Funktionale Programmierung/Ü5/Ü5.hs
+++ Universität/Funktionale Programmierung/Ü5/Ü5.hs
@@ -1,14 +1,31 @@
-import Data.List (sort)
+import Data.List (sort,nub)
+--1. Aufgabe
+toDecFrom :: Int -> [Int] -> Int
+toDecFrom b xs = foldl multBase 0 xs
+	where
+		multBase x y = (x*b)+y
+--Eingabewert: n=Länge der Liste xs
+--multBase ist konstant, kann daher als solche behandelt werden.
+--foldl ist von der Komplexität her linear.
+--O(n)
 
--- Aufgabe 1
-funktion b xs = foldl multBase 0 xs
-  where
-    multBase x y = x * b + y
+--2. Aufgabe
+--flatten_r im Gegensatz zu flatten_l ist viel komplizierter, da von rechts nach links
+--gerechnet wird und dabei alle Listen neu konstruiert werden müssen.
+flatten :: [[a]] -> [a]
+flatten xss = [y | ys <- xss, y <- ys]
 
 --3. Aufgabe
 potenzSeq :: Int -> Int -> [Int]
-potenzSeq n m = sort [b^e|b<-[2,3..n],e<-[2,3..m]]
---potenzSeq ist eine quadratische Funktion
+potenzSeq n m = sort nub [b^e|b<-[2,3..n],e<-[2,3..m]]
+--potenzSeq ist eine lineare Funktion
+--O(n*m)
 
 --4. Aufgabe
--- minNatNotIn [n] = [x|x<-[1..
+{-
+minNatNotIn :: [Int] -> Int
+minNatNotIn xs = [y|y<-[0..5],y/=xs] -}
+
+--5. Aufgabe
+--Eingabewert: n=Länge der Liste xs
+--O
